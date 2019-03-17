53742627e18838d830c6d9101971d08eb2ff88e7
Yohei Noguchi <uhei41594@gmail.com>
2019-03-18 00:51:40 +0900

    Preparation for distributing the framework

---
+++ Carthage/Build/iOS/TFAlertController.framework/Headers/TFAlertController.h
@@ -0,0 +1,18 @@
+//
+//  TFAlertController.h
+//  TFAlertController
+//
+//  Copyright © 2019 Yohei Noguchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TFAlertController.
+FOUNDATION_EXPORT double TFAlertControllerVersionNumber;
+
+//! Project version string for TFAlertController.
+FOUNDATION_EXPORT const unsigned char TFAlertControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TFAlertController/PublicHeader.h>
+
+
---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Copyright © 2019 Yohei Noguchi. All rights reserved.
+//
+
+import UIKit
+import TFAlertController
+
+class ViewController: UIViewController {
+    
+    var alertController: TFAlertController!;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let button = UIButton(frame: CGRect.zero);
+        button.setTitle("Alert", for: .normal);
+        button.setTitleColor(.blue, for: .normal);
+        button.sizeToFit();
+        button.center = CGPoint(x: self.view.frame.midX, y: self.view.frame.midY);
+        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside);
+        self.view.addSubview(button);
+        
+        let label = UILabel(frame: CGRect(x: 0, y: 0, width: self.view.frame.width - 40, height: 30));
+        label.font = UIFont.systemFont(ofSize: 25.0);
+        label.textAlignment = .center;
+        label.center = CGPoint(x: self.view.frame.midX, y: button.frame.maxY - 100);
+        self.view.addSubview(label);
+        
+        self.alertController = TFAlertController(title: "Alert", description: "The description shows here", handler: {(text: String) in
+            label.text = text;
+        });
+        
+        // You can customize all of the alert view's components; title label, description label, text field, "Done" button, "Cancel" button.
+        // For example
+        self.alertController.alertView.textField.text = "Default text";
+        self.alertController.alertView.doneButton.setTitleColor(.red, for: .normal);
+    }
+    
+    @objc func buttonTapped() {
+        self.present(self.alertController, animated: true, completion: nil);
+    }
+}
